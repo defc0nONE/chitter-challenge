11fb5bee8dcc41df537a11efcbe7e02925173256
Christopher Ly <demedial@gmail.com>
2018-03-17 16:58:37 +0000

    Gemfile set up, web infrastructure set up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
+require 'capybara'
+require 'capybara/rspec'
+require 'rake'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+Capybara.app = Chitter
+
+ENV['ENVIRONMENT'] = 'test'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
